41d76f28f4fe91fdad7393ae6128087595e5d4f8
Deji <38789915+dejisch@users.noreply.github.com>
2020-08-22 20:36:38 +0100

    update the environment

--- src/test/java/stepDefinitions/MyStepdefs1.java
+++ src/test/java/stepDefinitions/MyStepdefs1.java
@@ -12,12 +12,12 @@ public class MyStepdefs1 {
     //@Given("I navigate to blueskycitadelform site")
     public void i_navigate_to_blueskycitadelform_site()  {
         //STEP 2: set your system path
-        System.setProperty("webdriver.gecko.driver", "D:\\BlueskyCitadel\\firefox\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\BlueSky\\gecko\\geckodriver.exe");
 
         //STEP 3: Instantiate your firefox driver
         driver = new FirefoxDriver();
 
         //STEP 4: Navigate to url
-        driver.get("http://blueskycitadel.com/test-form-for-bluesky-automation-training/");
+        driver.get("http://blueskycitadel.com/");
     }
 }
